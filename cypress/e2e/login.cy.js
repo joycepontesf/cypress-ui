93a4e3e2f4a4eeaf792356df47f83d9f6236fe18
Joyce Pontes <60980108+joycepontesf@users.noreply.github.com>
2023-09-30 11:35:25 -0300

    Testes automatizados UI com Cypress

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress" />
+
+context('Funcionalidade de Login', () => {
+
+    beforeEach(() => {
+        cy.visit("minha-conta")
+    });
+
+    it('Deve fazer login bem sucedido', () => {
+        cy.login_sucesso("aluno_ebac@teste.com", "teste@teste.com")
+
+    });
+
+    it('Deve apresentar mensagem de erro para usuário inválido', () => {
+        cy.login_erro_usuario("234@teste.com", "teste@teste.com")
+    
+    });
+
+    it('Deve apresentar mensagem de erro para senha inválida', () => {
+       cy.login_erro_senha("aluno@teste.com", "teste@teste.com")
+        
+    });
+});
+
